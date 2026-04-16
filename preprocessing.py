98c8cde8d0b7fa4549e86fd0b8dd57fef4f07c9d
Pontus Gustafsson <pontg@chalmers.se>
2026-04-16 10:40:06 +0200

    preprocessing implementation

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+"""
+creates a matrix (x) of previous stock prices and a target vector (y) of prices the next day
+"""
+
+def create_matrix_target(df, window_size):
+    series = df["close"].values() #uses closed price
+    
+    x = []
+    y = []
+    
+    for i in range(len(series)-window_size):
+        window = series[i:i+window_size]
+        target = series[i+window_size]
+        
+        x.append(window)
+        y.append(target)
+    
+    return np.array(x), np.array(y)
+       
+       
+def standardize_data(x_train, x_test):
+    mean = np.mean(x_train, axis=0)
+    std = np.std(x_train,axis=0)
+    
+    std[std==0] = 1.0
+    
+    #uses z-score standardization
+    x_train_scaled = (x_train-mean)/std
+    x_test_scaled = (x_test-mean)/std
+    
+    return x_train_scaled, x_test_scaled, mean, std
